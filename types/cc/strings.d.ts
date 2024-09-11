d02a6022463384e2925e998816708c6939cee845
JackMacWindows <jackmacwindowslinux@gmail.com>
2024-09-11 17:23:37 -0400

    Update strings.d.ts

--- types/cc/strings.d.ts
+++ types/cc/strings.d.ts
@@ -3,4 +3,5 @@
 declare module "cc.strings" {
     export function wrap(text: string, width?: number): string[];
     export function ensure_width(text: string, width?: number): string;
+    export function split(str: string, deliminator: string, plain?: boolean, limit?: number): string[];
 }
